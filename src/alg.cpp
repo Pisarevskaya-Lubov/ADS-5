22d81b61d79b53215e5ddd0136f1ee567f19d367
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-04-09 09:09:17 +0300

    B123

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,112 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int priorGet(char symbol) {
+  switch (symbol) {
+  case '(':
+    return 0;
+  case ')':
+    return 1;
+  case '+':
+    return 2;
+  case '-':
+    return 2;
+  case '*':
+    return 3;
+  case '/':
+    return 3;
+  case ' ':
+    return -100;
+  }
+  return -1;
 }
 
-int eval(std::string pref) {
-  // добавьте код
+//std::string infx2pstfx(std::string inf) {
+  TStack<char, 100> stack;
+  std::string output = "";
+  int prior = 0;
+  int gratPrior = -1;
+  for (auto& op : inf) {
+    prior = priorGet(op);
+    gratPrior = priorGet(stack.get());
+    if (op == '\n') { 
+      while (priorGet(stack.get()) > 1) {
+        output += stack.pop();
+        output += " ";
+      }
+      stack.pop();
+    }
+    if (prior == -1) {
+      output += op;
+      output += " ";
+    }
+    else if (prior == 0) { 
+      stack.push(op);
+    }
+    else if (prior == 1) { 
+      while (priorGet(stack.get()) > 0) {
+        output += stack.pop();
+        output += " ";
+      }
+      stack.pop();
+    }
+    else if (prior > gratPrior || stack.isEmpty()) {
+      stack.push(op);
+    }
+    else if (prior <= gratPrior && prior > 1) {
+      while (priorGet(stack.get()) > 1) {
+        output += stack.pop();
+        output += " ";
+      }
+      stack.push(op);
+    }
+  }
+  while (priorGet(stack.get()) > 1) {
+    output += stack.pop();
+    output += " ";
+  }
+  std::string out = "";
+  for (int i = 0; i < output.length() - 1; ++i) {
+    out += output[i];
+  }
+  return out;
+}
+int opreoretat(int a, int b, char op) {
+  switch (op) {
+  case '+':
+    return a + b;
+  case '-':
+    return a - b;
+  case '*':
+    return a * b;
+  case '/':
+    return a / b;
+  }
   return 0;
 }
+int eval(std::string pref) {
+  TStack<int, 100> stack1;
+  int preoret = 0;
+  int prior = 0;
+  for (auto& op : pref) {
+    prior = priorGet(op);
+    if (prior == -100) { 
+      continue;
+    }
+    if (prior == -1) { 
+      stack1.push(op - 48);
+    }
+    else {
+      int hz = 0;
+      while (!(stack1.isEmpty() + hz)) {
+        hz += 1;
+        int a = stack1.pop();
+        int b = stack1.pop();
+        preoret = opreoretat(b, a, op);
+        stack1.push(preoret);
+      }
+    }
+  }
+  return stack1.pop();
+}
+
